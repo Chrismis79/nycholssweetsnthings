52e55aaef2ba8dcb3c2b872a1405a10a3a015145
Chrismis79 <chrismis79@gmail.com>
2019-11-30 14:05:46 -0600

    tweaked a few more styles

--- JS/scripts.js
+++ JS/scripts.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     $('#sho-cakes').click(function () {
         $('html, body').animate({
             scrollTop: $('#shower-cakes').offset().top
-        }, 1500);
+        }, 1650);
     });
 });
 
@@ -18,7 +18,7 @@ $(document).ready(function() {
     $('#spec-cakes').click(function () {
         $('html, body').animate({
             scrollTop: $('#specialty-cakes').offset().top
-        }, 1500);
+        }, 1800);
     });
 });
 
@@ -26,7 +26,7 @@ $(document).ready(function() {
     $('#PT-cakes').click(function () {
         $('html, body').animate({
             scrollTop: $('#party-cakes').offset().top
-        }, 1500);
+        }, 2000);
     });
 });
 
@@ -34,6 +34,38 @@ $(document).ready(function() {
     $('#any-cakes').click(function () {
         $('html, body').animate({
             scrollTop: $('#misc-cakes').offset().top
+        }, 2200);
+    });
+});
+
+$(document).ready(function() {
+    $('#BD-top').click(function () {
+        $('html, body').animate({
+            scrollTop: $('#top').offset().top
+        }, 1000);
+    });
+});
+
+$(document).ready(function() {
+    $('#sho-top').click(function () {
+        $('html, body').animate({
+            scrollTop: $('#top').offset().top
         }, 1500);
+    });
+});
+
+$(document).ready(function() {
+    $('#spec-top').click(function () {
+        $('html, body').animate({
+            scrollTop: $('#top').offset().top
+        }, 2000);
+    });
+});
+
+$(document).ready(function() {
+    $('#any-top').click(function () {
+        $('html, body').animate({
+            scrollTop: $('#top').offset().top
+        }, 2000);
     });
 });
