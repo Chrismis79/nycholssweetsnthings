b29717052a16636cd23740e2f16eaedc2ee5d259
Chrismis79 <chrismis79@gmail.com>
2019-11-30 13:20:07 -0600

    added gallery menu to scroll to selected section

---
+++ JS/scripts.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    $('#BD-cakes').click(function () {
+        $('html, body').animate({
+            scrollTop: $('#birthday-cakes').offset().top
+        }, 1500);
+    });
+});
+
+$(document).ready(function() {
+    $('#sho-cakes').click(function () {
+        $('html, body').animate({
+            scrollTop: $('#shower-cakes').offset().top
+        }, 1500);
+    });
+});
+
+$(document).ready(function() {
+    $('#spec-cakes').click(function () {
+        $('html, body').animate({
+            scrollTop: $('#specialty-cakes').offset().top
+        }, 1500);
+    });
+});
+
+$(document).ready(function() {
+    $('#PT-cakes').click(function () {
+        $('html, body').animate({
+            scrollTop: $('#party-cakes').offset().top
+        }, 1500);
+    });
+});
+
+$(document).ready(function() {
+    $('#any-cakes').click(function () {
+        $('html, body').animate({
+            scrollTop: $('#misc-cakes').offset().top
+        }, 1500);
+    });
+});
